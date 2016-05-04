123e24818b59d480d7176878a3952a1f7fc87855
Yulia <yuliamineeva@yandex.ru>
2016-05-04 14:20:07 +0500

    Add class Point

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,7 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+
+            p2.Draw();
+
             Console.ReadLine();
         }
     }
